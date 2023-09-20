513a3e4432fb77c0d2197f6eb99149236a37a6e0
kurakke <kcanggua@gmail.com>
2023-09-20 20:27:21 +0900

    ネストを解消

--- src/routes/socketRoutes.ts
+++ src/routes/socketRoutes.ts
@@ -5,18 +5,19 @@ import { RoomManager } from '../models/roomManager';
 export const setupSocket = (io: any) => {
     io.on('connection', (socket: any) => {
         socket.emit('kurakke', 'kurakke');
+        const roomManager = new RoomManager();
         socket.on('joinOrCreateRoom', (passphrase: string, nickname: string) => {
-            const roomManager = new RoomManager();
             const room = roomManager.joinOrCreateRoom(passphrase, nickname);
             socket.join(room.passphrase);
             socket.to(room.passphrase).emit('roomMembers',room.members)
-            socket.on('gamestart', (roomPassphrase: string) => {
-                const gameRoom = roomManager.findRoomByPassphrase(roomPassphrase);
-                if(gameRoom && room.canStartGame()) {
-                    io.to(roomPassphrase).emit('gamestart');
-                    game(socket, room);
-                }
-            })
+
         });
+        socket.on('gamestart', (roomPassphrase: string) => {
+            const gameRoom = roomManager.findRoomByPassphrase(roomPassphrase);
+            if(gameRoom && gameRoom.canStartGame()) {
+                io.to(roomPassphrase).emit('gamestart');
+                game(socket, gameRoom);
+            }
+        })
     });
 };
