2ef89039b7f79a0990f5581fc5c21e74fbffaff7
kurakke <kcanggua@gmail.com>
2023-09-20 15:05:08 +0900

    テーマのダミーデータを入れました

---
+++ src/utils/constants.ts
@@ -0,0 +1,26 @@
+export interface Thema {
+    id: string,
+    text: string,
+}
+export const thema:Thema[] = [
+    {
+        id: '08C4878D-C531-4C1E-9D05-566567A17052',
+        text: '自分の好きな言語'
+    },
+    {
+        id: '5B9CF232-039F-40C7-A13B-A62BA86183F2',
+        text: '静的型付け言語'
+    },
+    {
+        id: '35B76644-9AD7-4FD7-A810-28BAB7C348D3',
+        text: '動的型付け言語'
+    },
+    {
+        id: 'CD63AF0F-6432-442A-B4B9-6B1FF51CF79F',
+        text: 'Linuxのコマンド'
+    },
+    {
+        id: '7F87FEFF-F16D-43C6-A444-95BCFECF7F93',
+        text: '手続き型言語'
+    }
+]
