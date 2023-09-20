aedf40ffa50e8d0359142b446aa8dcd75ce5b16d
kurakke <kcanggua@gmail.com>
2023-09-20 15:20:32 +0900

    テーマを決めるようにした

--- src/controller/gameController.ts
+++ src/controller/gameController.ts
@@ -1,7 +1,10 @@
 import { Room } from "../models/room"
+import { themaSelect } from "../utils/themaSelect"
+import { Thema, thema } from "../utils/constants"
 
 export const game = (io: any, room: Room) => {
     const members = room.members
+    const selectedThema = themaSelect(thema)
     members.map((item, index) => {
         
     })
