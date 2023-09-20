e9f90bf21b55a3e090ccaa4fa2e95f4666ed7d6c
kurakke <kcanggua@gmail.com>
2023-09-20 17:11:15 +0900

    ゲームの関数作った

--- src/controller/gameController.ts
+++ src/controller/gameController.ts
@@ -2,10 +2,28 @@ import { Room } from "../models/room"
 import { themaSelect } from "../utils/themaSelect"
 import { Thema, thema } from "../utils/constants"
 
-export const game = (io: any, room: Room) => {
+export const game = (socket: any, room: Room) => {
     const members = room.members
     const selectedThema = themaSelect(thema)
-    members.map((item, index) => {
-        
-    })
+    const relayMessageToEachUser = members.map((member, index) => {
+        return async (prevMessage: string) => {
+            // 最初のメンバーには selectedThema を、それ以外のメンバーには前のメンバーからのメッセージを送る
+            const msgToSend = index === 0 ? selectedThema : prevMessage;
+            socket.to(member.id).emit('yourTurn', msgToSend);
+
+            // メンバーからの応答を待つ
+            return new Promise<string>((resolve) => {
+                socket.on(`response-${member.id}`, (message: string) => {
+                    resolve(message); // メンバーからのメッセージでPromiseを解決
+                });
+            });
+        };
+    });
+
+    relayMessageToEachUser.reduce(async (prevPromise, currentFunc) => {
+        const prevMessage = await prevPromise;
+        return currentFunc(prevMessage);
+    }, Promise.resolve('')).then(() => {
+        console.log('end');
+    });
 }
