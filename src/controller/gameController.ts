03f81a21db10143a65d077916baf49902fb7e96c
kurakke <kcanggua@gmail.com>
2023-09-21 02:24:02 +0900

    ゲームのログを取れるようにした

--- src/controller/gameController.ts
+++ src/controller/gameController.ts
@@ -1,10 +1,26 @@
 import { Room } from "../models/room"
 import { themaSelect } from "../utils/themaSelect"
-import { thema } from "../utils/constants"
+import { Thema, thema } from "../utils/constants"
+import { PrismaClient } from "@prisma/client"
 
 export const game = (socket: any, room: Room) => {
     const members = room.members
     const selectedThema = themaSelect(thema)
+    const prisma = new PrismaClient()
+    const gameLog = async (thema: Thema) => {
+        const res = await prisma.gamelog.create({
+            data: {
+                Thema: {
+                    create: {
+                        text: thema.text
+                    }
+                }
+
+            }
+        })
+        return res;
+    }
+    const GameLog = gameLog(selectedThema);
     const relayMessageToEachUser = members.map((member, index) => {
         return async (prevMessage: string) => {
             // 最初のメンバーには selectedThema を、それ以外のメンバーには前のメンバーからのメッセージを送る
@@ -13,7 +29,14 @@ export const game = (socket: any, room: Room) => {
 
             // メンバーからの応答を待つ
             return new Promise<string>((resolve) => {
-                socket.on(`response-${member.id}`, (message: string) => {
+                socket.on(`response-${member.id}`,async (message: string) => {
+                    await prisma.answer.create({
+                        data:{
+                            text:message,
+                            gamelog_id: (await GameLog).id,
+                            user_id: member.id,
+                        }
+                    })
                     resolve(message); // メンバーからのメッセージでPromiseを解決
                 });
             });
