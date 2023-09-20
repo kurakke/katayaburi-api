beaf58d1e7cf323f96822d10e11d55c3436bfb54
kurakke <kcanggua@gmail.com>
2023-09-21 02:04:37 +0900

    Promise型になってしまっていたのでDB叩く処理を一旦切り分け

--- src/utils/themaSelect.ts
+++ src/utils/themaSelect.ts
@@ -1,15 +1,9 @@
 import { Thema } from "./constants";
 import { PrismaClient } from "@prisma/client";
 
-export const themaSelect = async (thema: Thema[]) => {
-    const prisma = new PrismaClient();
+export const themaSelect = (thema: Thema[]) => {
     const randomIndex = Math.floor(Math.random() * thema.length);
-    const savedThema = await prisma.thema.create({
-        data: {
-            text: thema[randomIndex].text,
-        },
-    });
 
-    console.log(savedThema)
-    return savedThema.text
+    console.log(thema[randomIndex].text)
+    return thema[randomIndex].text
 }
